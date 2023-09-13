b9f6a67dd21fd842991841ceae4d1927ab82ff86
Arsalan Hadidi <arsalan.hadidi1379@gmail.com>
2023-09-14 02:02:57 +0430

    Initialized first commit of this repo

---
+++ Sardanapal.Identity.OTP/Models/Domain/OTPCode.cs
@@ -0,0 +1,39 @@
+﻿using Sardanapal.ModelBase.Model.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sardanapal.Identity.OTP.Models.Domain
+{
+    public interface IOTPCode<TKey>
+        : IBaseEntityModel<TKey>
+        where TKey : IComparable<TKey>, IEquatable<TKey>
+    {
+        string Code { get; set; }
+
+        TKey  UserId { get; set; }
+
+        DateTime ExpireTime { get; set; }
+
+        byte? Role { get; set; }
+    }
+
+    public class OTPCode<TKey> : BaseEntityModel<TKey>
+        where TKey : IComparable<TKey>, IEquatable<TKey>
+    {
+        [Required]
+        [MaxLength(6)]
+        public string Code { get; set; }
+
+        [Required]
+        public virtual TKey UserId { get; set; }
+
+        [Required]
+        public DateTime ExpireTime { get; set; }
+
+        public byte? Role { get; set; }
+    }
+}
---
+++ Sardanapal.Identity.Services/Service/RequestService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sardanapal.Identity.Services.Service
+{
+    public interface IRequestService
+    {
+        string Token { get; set; }
+    }
+
+    public class RequestService : IRequestService
+    {
+        public string Token { get; set; }
+
+        public RequestService(IHttpContextAccessor _http)
+        {
+            Token = _http.HttpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "").Trim();
+        }
+    }
+}
