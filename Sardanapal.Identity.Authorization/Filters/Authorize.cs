272504accfe35d56bcab736aaac9da4b675e3065
Arsalan Hadidi <arsalan.hadidi1379@gmail.com>
2024-03-08 12:15:10 +0330

    Add: Impelmented authorization filters

---
+++ Sardanapal.Identity.Authorization/Filters/Authorize.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Sardanapal.Identity.Services.Services;
+
+namespace Sardanapal.Identity.Authorization.Filters;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+public class AuthorizeAttribute : ActionFilterAttribute
+{
+    protected ITokenService _tokenService;
+    public AuthorizeAttribute(ITokenService tokenService)
+    {
+        _tokenService = tokenService;
+    }
+
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        base.OnActionExecuting(context);
+
+        try
+        {
+            string token = context.HttpContext.Request.Headers
+                .Where(h => h.Key == "Auth")
+                .Select(h => h.Value)
+                .FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                context.Result = new UnauthorizedResult();
+            }
+            else
+            {
+                if (!_tokenService.ValidateToken(token))
+                {
+                    context.Result = new UnauthorizedResult();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            context.Result = new UnauthorizedResult();
+        }
+    }
+
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        try
+        {
+            string token = context.HttpContext.Request.Headers
+                .Where(h => h.Key == "Auth")
+                .Select(h => h.Value)
+                .FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                context.Result = new UnauthorizedResult();
+            }
+            else
+            {
+                if (!_tokenService.ValidateToken(token))
+                {
+                    context.Result = new UnauthorizedResult();
+                }
+                else
+                {
+                    await next();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            context.Result = new UnauthorizedResult();
+        }
+    }
+}
---
+++ Sardanapal.Identity.Authorization/Filters/HasRole.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Sardanapal.Identity.Services.Services;
+
+namespace Sardanapal.Identity.Authorization.Filters;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+public class HasRoleAttribute : ActionFilterAttribute
+{
+    protected byte[] roleIds;
+    public HasRoleAttribute(byte[] _roleIds)
+    {
+        roleIds = _roleIds;
+    }
+
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        base.OnActionExecuting(context);
+
+        try
+        {
+            ITokenService tokenService = context.HttpContext.RequestServices.GetService(typeof(ITokenService)) as ITokenService;
+
+            string token = context.HttpContext.Request.Headers
+                .Where(h => h.Key == "Auth")
+                .Select(h => h.Value)
+                .FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                context.Result = new UnauthorizedResult();
+            }
+            else
+            {
+                if (!roleIds.Any(r => tokenService.ValidateTokenRole(token, r)))
+                {
+                    context.Result = new UnauthorizedResult();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            context.Result = new UnauthorizedResult();
+        }
+    }
+
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        try
+        {
+            ITokenService tokenService = context.HttpContext.RequestServices.GetService(typeof(ITokenService)) as ITokenService;
+
+            string token = context.HttpContext.Request.Headers
+                .Where(h => h.Key == "Auth")
+                .Select(h => h.Value)
+                .FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                context.Result = new UnauthorizedResult();
+            }
+            else
+            {
+                if (!roleIds.Any(r => tokenService.ValidateTokenRole(token, r)))
+                {
+                    context.Result = new UnauthorizedResult();
+                }
+                else
+                {
+                    await next();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            context.Result = new UnauthorizedResult();
+        }
+    }
+}
