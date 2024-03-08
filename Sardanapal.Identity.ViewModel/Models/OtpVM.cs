178db4e5c3697120184ba751dbf823ef5b3a14bb
Arsalan Hadidi <arsalan.hadidi1379@gmail.com>
2024-03-08 15:51:42 +0330

    Add: Added new viewModels for otp service

---
+++ Sardanapal.Identity.ViewModel/Models/OtpVM.cs
@@ -0,0 +1,28 @@
+﻿using Sardanapal.ViewModel.Models;
+
+namespace Sardanapal.Identity.ViewModel.Models;
+
+public class OtpListItemVM<TKey> : BaseListItem<TKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
+{
+}
+
+public class OtpSearchVM
+{
+
+}
+
+public class NewOtpVM
+{
+
+}
+
+public class OtpEditableVM : NewOtpVM
+{
+
+}
+
+public class OtpVM
+{
+
+}
