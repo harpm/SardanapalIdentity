2edf9c78ba129e892be7340ca7214ec53efb9d72
Arsalan Hadidi <arsalan.hadidi1379@gmail.com>
2024-03-08 15:30:45 +0330

    Refactor: Updated OtpService and OtpHelper

--- Sardanapal.Identity.OTP/Services/CachService.cs
+++ Sardanapal.Identity.OTP/Services/CachService.cs
@@ -6,6 +6,31 @@ using Sardanapal.RedisCache.Models;
 
 namespace Sardanapal.Identity.OTP.Services;
 
+public interface IOtpHelper
+{
+    int OtpLength { get; }
+    string GenerateNewOtp();
+}
+
+public class OtpHelper : IOtpHelper
+{
+    protected int _otpLength;
+    public int OtpLength { get { return _otpLength; } }
+
+    public OtpHelper(int otpLength)
+    {
+        _otpLength = otpLength;
+    }
+
+    public string GenerateNewOtp()
+    {
+        char[] nines = new char[OtpLength];
+        Array.Fill(nines, '9');
+        return Random.Shared.Next(Convert.ToInt32(string.Join("", nines)))
+            .ToString("D" + OtpLength);
+    }
+}
+
 public interface IOtpCachService<TKey, TOtpCachModel> : ICacheService<TKey, TOtpCachModel>
     where TKey : IComparable<TKey>, IEquatable<TKey>
     where TOtpCachModel : OtpCachModel<TKey>, new()
@@ -17,14 +42,16 @@ public class OtpCachService<TKey, TOtpCachModel> : CacheService<TKey, TOtpCachMo
     where TKey : IComparable<TKey>, IEquatable<TKey>
     where TOtpCachModel : OtpCachModel<TKey>, new()
 {
-    public OtpCachService(IConnectionMultiplexer _conn, int _expireTime) : base(_conn, _expireTime)
+    protected IOtpHelper otpHelper {  get; set; }
+    public OtpCachService(IConnectionMultiplexer _conn, IOtpHelper _otpHelper, int _expireTime) : base(_conn, _expireTime)
     {
-
+        otpHelper = _otpHelper;
     }
 
     public override Task<CacheResponse<TKey>> Add(TOtpCachModel Model)
     {
         Model.ExpireTime = DateTime.UtcNow.AddMinutes(base.expireTime);
+        Model.Code = otpHelper.GenerateNewOtp();
 
         return base.Add(Model);
     }
