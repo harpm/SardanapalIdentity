536ab0c52d34922b2d1ffc0c53ad51e7c6f06baa
Arsalan Hadidi <arsalan.hadidi1379@gmail.com>
2024-03-08 16:08:59 +0330

    Add: Impelmented OtpService

--- Sardanapal.Identity.OTP/Services/CachService.cs
+++ Sardanapal.Identity.OTP/Services/CachService.cs
@@ -6,31 +6,6 @@ using Sardanapal.RedisCache.Models;
 
 namespace Sardanapal.Identity.OTP.Services;
 
-public interface IOtpHelper
-{
-    int OtpLength { get; }
-    string GenerateNewOtp();
-}
-
-public class OtpHelper : IOtpHelper
-{
-    protected int _otpLength;
-    public int OtpLength { get { return _otpLength; } }
-
-    public OtpHelper(int otpLength)
-    {
-        _otpLength = otpLength;
-    }
-
-    public string GenerateNewOtp()
-    {
-        char[] nines = new char[OtpLength];
-        Array.Fill(nines, '9');
-        return Random.Shared.Next(Convert.ToInt32(string.Join("", nines)))
-            .ToString("D" + OtpLength);
-    }
-}
-
 public interface IOtpCachService<TKey, TOtpCachModel> : ICacheService<TKey, TOtpCachModel>
     where TKey : IComparable<TKey>, IEquatable<TKey>
     where TOtpCachModel : OtpCachModel<TKey>, new()
---
+++ Sardanapal.Identity.OTP/Services/Service.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Sardanapal.Identity.OTP.Models.Domain;
+using Sardanapal.Identity.ViewModel.Models;
+using Sardanapal.InterfacePanel.Service;
+
+namespace Sardanapal.Identity.OTP.Services;
+
+public interface IOtpService<TKey, TListItemVM, TSearchVM, TVM, TNewVM, TEditableVM> : _IServicePanel<TKey, TListItemVM, TSearchVM, TVM, TNewVM, TEditableVM>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
+    where TListItemVM : OtpListItemVM<TKey>
+    where TSearchVM : OtpSearchVM
+    where TVM : OtpVM
+    where TNewVM : NewOtpVM
+    where TEditableVM : OtpEditableVM
+{
+
+}
+
+public class OtpService<UnitOfWork, TKey, OtpEntity, TListItemVM, TSearchVM, TVM, TNewVM, TEditableVM> : _ServicePanel<UnitOfWork, TKey, OtpEntity, TListItemVM, TSearchVM, TVM, TNewVM, TEditableVM>
+    where UnitOfWork : DbContext
+    where TKey : IComparable<TKey>, IEquatable<TKey>
+    where OtpEntity : OTPCode<TKey>
+    where TListItemVM : OtpListItemVM<TKey>
+    where TSearchVM : OtpSearchVM
+    where TVM : OtpVM
+    where TNewVM : NewOtpVM
+    where TEditableVM : OtpEditableVM
+{
+    public OtpService(UnitOfWork unitOfWork, IMapper _Mapper, IRequestService _Context)
+        : base(unitOfWork, _Mapper, _Context)
+    {
+
+    }
+}
