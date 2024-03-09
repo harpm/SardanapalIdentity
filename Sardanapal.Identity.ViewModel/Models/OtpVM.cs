db9afb1ed6f8739cf13870c5d7ba464d213fef61
Arsalan Hadidi <arsalan.hadidi1379@gmail.com>
2024-03-09 23:23:07 +0330

    Add: Added new fields in the OtpVM

--- Sardanapal.Identity.ViewModel/Models/OtpVM.cs
+++ Sardanapal.Identity.ViewModel/Models/OtpVM.cs
@@ -5,24 +5,31 @@ namespace Sardanapal.Identity.ViewModel.Models;
 public class OtpListItemVM<TKey> : BaseListItem<TKey>
     where TKey : IComparable<TKey>, IEquatable<TKey>
 {
+    
 }
 
-public class OtpSearchVM
+public abstract class OtpSearchVM
 {
 
 }
 
-public class NewOtpVM
+public abstract class NewOtpVM<TKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
+{
+    public string Code { get; set; }
+    public TKey UserId { get; set; }
+    public DateTime ExpireTime { get; set; }
+    public byte RoleId { get; set; }
+}
+
+public abstract class OtpEditableVM<TKey> : NewOtpVM<TKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
 {
 
 }
 
-public class OtpEditableVM : NewOtpVM
-{
-
-}
-
-public class OtpVM
+public abstract class OtpVM<TKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
 {
 
 }
