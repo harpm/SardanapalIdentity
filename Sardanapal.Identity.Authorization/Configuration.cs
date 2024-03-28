82e80bc78262691715e4216721b8e48be332bec7
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-03-29 02:33:51 +0330

    Add: Configuration class to auth package

---
+++ Sardanapal.Identity.Authorization/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sardanapal.Identity.Services.Services;
+
+namespace Sardanapal.Identity.Authorization;
+
+public static class Configuration
+{
+    public static IServiceCollection AddAuthServices<TTokenService>(this IServiceCollection services)
+        where TTokenService : class, ITokenService
+    {
+        return services.AddScoped<ITokenService, TTokenService>();
+    }
+}
