faa1480a79d185a80b99e5da00912fc8bebd1020
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-04-11 17:51:49 +0330

    Add: OtpModel package added

---
+++ Sardanapal.Identity.OTP.Model/Models/Domain/OTPModel.cs
@@ -0,0 +1,32 @@
+﻿using Sardanapal.DomainModel.Domain;
+using Sardanapal.RedisCach.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sardanapal.Identity.OTP.Models.Domain;
+
+public interface IOTPModel<TUserKey> : IBaseEntityModel<Guid>, ICachModel<Guid>
+    where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+{
+    string Code { get; set; }
+
+    TUserKey UserId { get; set; }
+
+    DateTime ExpireTime { get; set; }
+
+    byte? Role { get; set; }
+}
+
+public class OTPModel<TUserKey> : BaseEntityModel<Guid>, IOTPModel<TUserKey>
+    where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+{
+    [Required]
+    public virtual string Code { get; set; }
+
+    [Required]
+    public virtual TUserKey UserId { get; set; }
+
+    [Required]
+    public virtual DateTime ExpireTime { get; set; }
+
+    public virtual byte? Role { get; set; }
+}
---
+++ Sardanapal.Identity.OTP.Model/Models/VM/CachModel.cs
@@ -0,0 +1,34 @@
+﻿using Sardanapal.RedisCach.Models;
+using Sardanapal.ViewModel.Models;
+
+namespace Sardanapal.Identity.OTP.Model.Models.VM;
+
+public class CachOtpListItemVM<TUserKey> : BaseListItem<Guid>, ICachModel<Guid>
+    where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+{
+    public TUserKey UserId { get; set; }
+    public DateTime ExpireTime { get; set; }
+}
+
+public class CachNewOtpVM<TUserKey> : ICachModel<Guid>
+    where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+{
+    public Guid Id { get; set; }
+    public string Code { get; set; }
+    public TUserKey UserId { get; set; }
+    public DateTime ExpireTime { get; set; }
+    public byte RoleId { get; set; }
+}
+
+public class CachOtpEditableVM<TUserKey> : NewOtpVM<TUserKey>, ICachModel<Guid>
+    where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+{
+    public Guid Id { get; set; }
+}
+
+public class CachOtpVM<TUserKey> : ICachModel<Guid>
+    where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+{
+    public Guid Id { get; set; }
+    public TUserKey UserId { get; set; }
+}
