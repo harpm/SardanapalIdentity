539b9135734dcdfbf2e1552765d366a28881ea80
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-04-18 13:16:39 +0330

    Update: OtpCach ViewModels with consideration of services

--- Sardanapal.Identity.OTP.Model/Models/VM/CachModel.cs
+++ Sardanapal.Identity.OTP.Model/Models/VM/CachModel.cs
@@ -3,32 +3,36 @@ using Sardanapal.ViewModel.Models;
 
 namespace Sardanapal.Identity.OTP.Model.Models.VM;
 
-public class CachOtpListItemVM<TUserKey> : BaseListItem<Guid>, ICachModel<Guid>
+public class CachOtpListItemVM<TUserKey, TKey> : BaseListItem<TKey>, ICachModel<TKey>
     where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
 {
     public TUserKey UserId { get; set; }
     public DateTime ExpireTime { get; set; }
 }
 
-public class CachNewOtpVM<TUserKey> : ICachModel<Guid>
+public class CachNewOtpVM<TUserKey, TKey> : ICachModel<TKey>
     where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
 {
-    public Guid Id { get; set; }
+    public TKey Id { get; set; }
     public string Code { get; set; }
     public TUserKey UserId { get; set; }
     public DateTime ExpireTime { get; set; }
     public byte RoleId { get; set; }
 }
 
-public class CachOtpEditableVM<TUserKey> : NewOtpVM<TUserKey>, ICachModel<Guid>
+public class CachOtpEditableVM<TUserKey, TKey> : NewOtpVM<TKey>, ICachModel<TKey>
     where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
 {
-    public Guid Id { get; set; }
+    public TKey Id { get; set; }
 }
 
-public class CachOtpVM<TUserKey> : ICachModel<Guid>
+public class CachOtpVM<TUserKey, TKey> : ICachModel<TKey>
     where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
 {
-    public Guid Id { get; set; }
+    public TKey Id { get; set; }
     public TUserKey UserId { get; set; }
 }
