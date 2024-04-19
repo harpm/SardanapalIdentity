aff702efc59e73b54db5da1b33c7cb2e9e1a1665
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-04-19 22:03:22 +0330

    Fix: IdentityHolder

---
+++ Sardanapal.Identity.Authorization.Data/IdentityHolder.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Sardanapal.Identity.Authorization.Data;
+
+public interface IIdentityHolder
+{
+    bool IsAuthorized { get; }
+
+    void SetUserId(string userId);
+    void SetToken(string token);
+    string GetToken();
+    void SetAuthorize();
+}
