9e0550552f91173661ae4eddae6d1c6550b229d3
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-04-19 22:13:23 +0330

    Update: Identity Domain package

--- Sardanapal.Identity.Domain/Data/SdIdentityUnitOfWork.cs
+++ Sardanapal.Identity.Domain/Data/SdIdentityUnitOfWork.cs
@@ -1,26 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Sardanapal.Identity.Domain.Model;
-using Sardanapal.ModelBase.UnitOfWork;
+using Sardanapal.DomainModel.UnitOfWork;
 
 namespace Sardanapal.Identity.Domain.Data;
 
-public abstract class SdIdentityUnitOfWorkBase<TKey, TUser, TRole, TUR> : DbContext, IUnitOfWork
+public interface ISdIdentityUnitOfWorkBase<TKey, TUser, TRole, TUR> : ISardanapalUnitOfWork
     where TKey : IComparable<TKey>, IEquatable<TKey>
     where TUser : class, IUserBase<TKey>, new()
     where TRole : class, IRoleBase<byte>, new()
     where TUR : class, IUserRoleBase<TKey>, new()
+{
+    DbSet<TUser> Users { get; set; }
+    DbSet<TRole> Roles { get; set; }
+    DbSet<TUR> UserRoles { get; set; }
+}
+
+public abstract class SdIdentityUnitOfWorkBase<TUserKey, TUser, TRole, TUR> : SardanapalUnitOfWork
+    , ISdIdentityUnitOfWorkBase<TUserKey, TUser, TRole, TUR>
+    where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+    where TUser : class, IUserBase<TUserKey>, new()
+    where TRole : class, IRoleBase<byte>, new()
+    where TUR : class, IUserRoleBase<TUserKey>, new()
 {
     public DbSet<TUser> Users { get; set; }
     public DbSet<TRole> Roles { get; set; }
     public DbSet<TUR> UserRoles { get; set; }
-}
-
-public class SdIdentityUnitOfWork<TKey, TUser, TRole, TUR> : SdIdentityUnitOfWorkBase<TKey, TUser, TRole, TUR>
-    , IUnitOfWork
-    where TKey : IComparable<TKey>, IEquatable<TKey>
-    where TUser : class, IUserBase<TKey>, new()
-    where TRole : class, IRoleBase<byte>, new()
-    where TUR : class, IUserRoleBase<TKey>, new()
-{
-
+    
 }
