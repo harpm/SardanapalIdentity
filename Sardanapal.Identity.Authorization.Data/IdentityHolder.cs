67d5cb1f156716c1af2e02740df4749af76f821d
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-04-19 22:20:14 +0330

    Update: Authorization Data package

--- Sardanapal.Identity.Authorization.Data/IdentityHolder.cs
+++ Sardanapal.Identity.Authorization.Data/IdentityHolder.cs
@@ -1,10 +1,12 @@
 ﻿
+using System.Security.Claims;
+
 namespace Sardanapal.Identity.Authorization.Data;
 
 public interface IIdentityHolder
 {
+    ClaimsPrincipal Principals { get; }
     bool IsAuthorized { get; }
-
     void SetUserId(string userId);
     void SetToken(string token);
     string GetToken();
