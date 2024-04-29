69bc7b9f0fae97beecaa3ac794c4e960a7da68d6
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-04-29 12:52:28 +0330

    Update: Share package and added configuration

---
+++ Sardanapal.Identity.Share/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Sardanapal.Identity.Share;
+
+public static class Configuration
+{
+    public static IServiceCollection ConfigureIdentityOptions(this IServiceCollection services)
+    {
+        return services;
+    }
+}
