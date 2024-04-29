c586bd6ef8a0a409569deed8b09628edb38ce06b
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-04-29 13:39:30 +0330

    Update: Auth package

--- Sardanapal.Identity.Authorization/Configuration.cs
+++ Sardanapal.Identity.Authorization/Configuration.cs
@@ -8,6 +8,8 @@ public static class Configuration
     public static IServiceCollection AddAuthServices<TTokenService>(this IServiceCollection services)
         where TTokenService : class, ITokenService
     {
-        return services.AddScoped<ITokenService, TTokenService>();
+        services.AddHttpContextAccessor();
+        services.AddScoped<ITokenService, TTokenService>();
+        return services;
     }
 }
