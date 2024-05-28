c27afefeafa8a499b1e53497fb1c29dc2831b4e6
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-05-28 21:03:38 +0330

    Fix: issue in middleware DI logic

--- Sardanapal.Identity.Authorization/Middlewares/SdAuthorizationMiddleware.cs
+++ Sardanapal.Identity.Authorization/Middlewares/SdAuthorizationMiddleware.cs
@@ -1,37 +1,33 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
 using Sardanapal.Identity.Authorization.Data;
 using Sardanapal.Identity.Services.Services;
 using Sardanapal.ViewModel.Response;
-using System.Security.Claims;
 
 namespace Sardanapal.Identity.Authorization.Middlewares;
 
 public class SdAuthorizationMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly ITokenService _tokenService;
-    private readonly IIdentityHolder _identityHolder;
-
-    public SdAuthorizationMiddleware(RequestDelegate next, ITokenService tokenService, IIdentityHolder identityHolder)
+    
+    public SdAuthorizationMiddleware(RequestDelegate next)
     {
         _next = next;
-        _tokenService = tokenService;
-        _identityHolder = identityHolder;
     }
 
-    public async Task InvokeAsync(HttpContext context)
+    public async Task InvokeAsync(HttpContext context, ITokenService tokenService, IIdentityHolder identityHolder)
     {
         string token = context.Request.Headers
             .Where(x => x.Key.Equals("Auth", StringComparison.InvariantCultureIgnoreCase))
             .Select(x => x.Value)
             .FirstOrDefault();
 
-        var res = _tokenService.ValidateToken(token, out ClaimsPrincipal cp);
+        var res = tokenService.ValidateToken(token, out ClaimsPrincipal cp);
         if (res.StatusCode == StatusCode.Succeeded && res.Data)
         {
-            _identityHolder.SetUserId(cp.FindFirst(ClaimTypes.NameIdentifier).Value);
-            _identityHolder.SetToken(token);
-            _identityHolder.SetAuthorize();
+            identityHolder.SetUserId(cp.FindFirst(ClaimTypes.NameIdentifier).Value);
+            identityHolder.SetToken(token);
+            identityHolder.SetAuthorize();
         }
 
         // Call the next delegate/middleware in the pipeline.
