7288e04c575931b8b1a2090801be297207dedf09
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-06-06 14:45:12 +0330

    Update: fixed identity info option configs

--- Sardanapal.Identity.Share/Configuration.cs
+++ Sardanapal.Identity.Share/Configuration.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Sardanapal.Identity.Share.Options;
 
 namespace Sardanapal.Identity.Share;
 
 public static class Configuration
 {
-    public static IServiceCollection ConfigureIdentityOptions(this IServiceCollection services)
+    public static IServiceCollection ConfigureIdentityOptions(this IServiceCollection services
+        , string connectionString
+        , string redisConnection
+        , TokenValidationParameters validationParams
+        , int otpExpTime
+        , int otpLength)
     {
-        services.AddSingleton<IdentityInfo>();
+        services.AddOptions<IdentityInfo>().Configure(i =>
+        {
+            i.ConnectionString = connectionString;
+            i.RedisConnectionString = redisConnection;
+            i.OTPLength = otpLength;
+            i.TokenParameters = validationParams;
+            i.ExpirationTime = otpExpTime;
+        });
         return services;
     }
 }
--- Sardanapal.Identity.Share/Options/IdentityInfo.cs
+++ Sardanapal.Identity.Share/Options/IdentityInfo.cs
@@ -1,6 +1,4 @@
-﻿using System.Text;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.IdentityModel.Tokens;
 
 namespace Sardanapal.Identity.Share.Options;
 
@@ -11,22 +9,5 @@ public class IdentityInfo
     public TokenValidationParameters TokenParameters { get; set; }
     public int ExpirationTime { get; set; }
     public int? OTPLength { get; set; }
-
-    public IdentityInfo(IConfiguration config)
-    {
-        ConnectionString = config.GetConnectionString("Main");
-        RedisConnectionString = config.GetConnectionString("Redis");
-        var TokenProvider = config.GetSection("TokenProvider");
-        string secretKeyStr = TokenProvider.GetSection("SecretKey").Value ?? "";
-        var SymmetricKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKeyStr));
-        TokenParameters = new TokenValidationParameters()
-        {
-            ValidIssuer = TokenProvider.GetSection("Issuer").Value,
-            ValidAudience = TokenProvider.GetSection("Audience").Value,
-            IssuerSigningKey = SymmetricKey
-        };
-        ExpirationTime = Convert.ToInt32(TokenProvider.GetSection("TokenExpireTime").Value);
-        OTPLength = Convert.ToInt32(TokenProvider.GetSection("OtpLength").Value);
-    }
 }
 
