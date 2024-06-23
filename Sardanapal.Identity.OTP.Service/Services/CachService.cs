c07ba90093320a3dfb8032b381eda3880860fffa
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-06-23 17:23:08 +0330

    Add: validation for otp cach service

--- Sardanapal.Identity.OTP.Service/Services/CachService.cs
+++ Sardanapal.Identity.OTP.Service/Services/CachService.cs
@@ -9,7 +9,7 @@ using Sardanapal.Identity.OTP.Domain;
 namespace Sardanapal.Identity.OTP.Services;
 
 public interface IOtpCachService<TUserKey, TKey, TOtpCachModel>
-    : ICacheService<TKey, OtpSearchVM, CachOtpVM<TUserKey, TKey>, CachNewOtpVM<TUserKey, TKey>, CachOtpEditableVM<TUserKey, TKey>>
+    : ICacheService<TOtpCachModel, TKey, OtpSearchVM, CachOtpVM<TUserKey, TKey>, CachNewOtpVM<TUserKey, TKey>, CachOtpEditableVM<TUserKey, TKey>>
     , IOtpServiceBase<TUserKey, TKey, CachNewOtpVM<TUserKey, TKey>, ValidateOtpVM<TUserKey>>
     where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
     where TKey : IComparable<TKey>, IEquatable<TKey>
@@ -19,13 +19,14 @@ public interface IOtpCachService<TUserKey, TKey, TOtpCachModel>
 }
 
 public class OtpCachService<TUserKey, TKey, TOtpCachModel>
-    : CacheService<OTPModel<TUserKey, TKey>, TKey, OtpSearchVM, CachOtpVM<TUserKey, TKey>, CachNewOtpVM<TUserKey, TKey>, CachOtpEditableVM<TUserKey, TKey>>
+    : CacheService<TOtpCachModel, TKey, OtpSearchVM, CachOtpVM<TUserKey, TKey>, CachNewOtpVM<TUserKey, TKey>, CachOtpEditableVM<TUserKey, TKey>>
     , IOtpCachService<TUserKey, TKey, TOtpCachModel>
     where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
     where TKey : IComparable<TKey>, IEquatable<TKey>
     where TOtpCachModel : OTPModel<TUserKey, TKey>, new()
 {
     protected override string key => "Otp";
+    public override string ServiceName => "OtpService";
 
     public int expireTime
     {
@@ -46,17 +47,31 @@ public class OtpCachService<TUserKey, TKey, TOtpCachModel>
         otpHelper = _otpHelper;
     }
 
-    public override Task<IResponse<TKey>> Add(CachNewOtpVM<TUserKey, TKey> Model)
+    public override Task<IResponse<TKey>> Add(CachNewOtpVM<TUserKey, TKey> model)
     {
-        Model.ExpireTime = DateTime.UtcNow.AddMinutes(base.expireTime);
-        Model.Code = otpHelper.GenerateNewOtp();
+        model.ExpireTime = DateTime.UtcNow.AddMinutes(base.expireTime);
+        model.Code = otpHelper.GenerateNewOtp();
 
-        return base.Add(Model);
+        return base.Add(model);
     }
 
-    public Task<IResponse<bool>> ValidateOtp(ValidateOtpVM<TUserKey> model)
+    public async Task<IResponse<bool>> ValidateOtp(ValidateOtpVM<TUserKey> model)
     {
-        throw new NotImplementedException();
+        IResponse<bool> result = new Response<bool>(ServiceName, OperationType.Fetch);
+        return await result.FillAsync(async () =>
+        {
+            var items = await InternalGetAll();
+            if (items != null)
+            {
+                result.Set(StatusCode.Succeeded
+                    , items.Where(i => i.UserId.Equals(model.UserId) && i.Code == model.Code && i.Role == model.RoleId)
+                    .Any());
+            }
+            else
+            {
+                result.Set(StatusCode.NotExists);
+            }
+        });
     }
 
     /// <summary>
