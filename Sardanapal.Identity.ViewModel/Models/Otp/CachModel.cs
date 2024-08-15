20fb99bafc0506ce18b7bb54742be79bf6efb120
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-08-15 14:53:49 +0330

    move: otp vms to the viewmodel proj

---
+++ Sardanapal.Identity.ViewModel/Models/Otp/CachModel.cs
@@ -0,0 +1,34 @@
+﻿using Sardanapal.RedisCach.Models;
+
+namespace Sardanapal.Identity.ViewModel.Otp;
+
+public class CachOtpListItemVM<TUserKey, TKey> : OtpListItemVM<TKey>
+    , ICachModel<TKey>
+    where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
+{
+    public TUserKey UserId { get; set; }
+    public override DateTime ExpireTime { get; set; }
+}
+
+public class CachNewOtpVM<TUserKey, TKey> : NewOtpVM<TUserKey>, ICachModel<TKey>
+    where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
+{
+    public TKey Id { get; set; }
+}
+
+public class CachOtpEditableVM<TUserKey, TKey> : OtpEditableVM<TUserKey>, ICachModel<TKey>
+    where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
+{
+    public TKey Id { get; set; }
+}
+
+public class CachOtpVM<TUserKey, TKey> : OtpVM, ICachModel<TKey>
+    where TUserKey : IComparable<TUserKey>, IEquatable<TUserKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
+{
+    public TKey Id { get; set; }
+    public TUserKey UserId { get; set; }
+}
